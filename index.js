e29b1f6056a239b712afc3b5058f06ca9e5b8106
MattIPv4 <me@mattcowley.co.uk>
2019-09-07 16:37:47 +0100

    Render the markdown descs

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+/**
+ *  Personal Site: My humble personal homepage, made with a tiny bit but not much care.
+ *  <https://github.com/MattIPv4/Personal-Site/>
+ *  Copyright (C) 2019 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/MattIPv4/Personal-Site/blob/master/LICENSE.md> or <http://www.gnu.org/licenses/>.
+ */
+
 const { readFileSync, writeFileSync } = require('fs');
 const { parse } = require('yaml');
+const md = require('markdown-it')();
 
 const toolMap = {
     'phpstorm': 'devicon-phpstorm-plain',
@@ -62,6 +81,9 @@ const projects = Object.keys(yaml).map(key => {
             "name": tool
         };
     });
+
+    // Markdown
+    data.desc = md.renderInline(data.desc).replace(/<a (.+?)>/g, '<a $1 target="_blank" rel="noopener">');
 
     return data;
 }).filter(x => x.display);
