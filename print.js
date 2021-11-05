aa43160c1ac518016cbf54dd22770bb8265cf2d2
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2021-11-05 23:48:45 +0000

    Print environment (#24)
    * Start adding logic for a print environment
    
    * Bump to Node.js 16
    
    * Building out the print environment
    
    * Set python version for Cf pages
    
    * Update wording for DO project
    
    * Updated info
    
    * Add ability to export print version
    
    * Run print build as part of build workflow
    
    * Linting fixes
    
    * Pupeteer sandbox bails on Cf Pages

---
+++ print.js
@@ -0,0 +1,37 @@
+/**
+ *  Personal Site: My humble personal homepage, made with a tiny bit but not much care.
+ *  <https://github.com/MattIPv4/Personal-Site/>
+ *  Copyright (C) 2021 Matt Cowley (MattIPv4) (me@mattcowley.co.uk)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. If not, please see
+ *   <https://github.com/MattIPv4/Personal-Site/blob/master/LICENSE.md> or <http://www.gnu.org/licenses/>.
+ */
+
+const path = require('path');
+const html5ToPdf = require('html5-to-pdf');
+
+const run = async () => {
+    const html5ToPDF = new html5ToPdf({
+        inputPath: path.join(__dirname, 'build', 'print.html'),
+        outputPath: path.join(__dirname, 'build', 'print.pdf'),
+        launchOptions: { args: [ '--no-sandbox' ] }
+    });
+
+    await html5ToPDF.start();
+    await html5ToPDF.build();
+    await html5ToPDF.close();
+};
+
+run().then(() => console.log('Exported')).catch(err => {
+    console.error(err);
+    process.exit(1);
+});
