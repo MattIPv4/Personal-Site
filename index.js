51fbfe58fb2eb1f8341156ccd63e7217cb9419d1
MattIPv4 <me@mattcowley.co.uk>
2022-04-30 02:06:11 +0100

    Replace parcel with sass + esbuild

--- index.js
+++ index.js
@@ -16,63 +16,158 @@
  *   <https://github.com/MattIPv4/Personal-Site/blob/master/LICENSE.md> or <http://www.gnu.org/licenses/>.
  */
 
-const path = require('path');
-const { readFileSync, writeFileSync } = require('fs');
-const { parse } = require('yaml');
-const md = require('markdown-it')();
+// Node
+const { join, dirname } = require('node:path');
+const { readFileSync, writeFileSync, readdirSync, existsSync, mkdirSync } = require('node:fs');
+
+// SCSS
+const sass = require('sass');
+
+// JS
+const esbuild = require('esbuild');
+const esbuildYaml = require('esbuild-plugin-yaml');
+
+// HTML
+const yaml = require('yaml');
+const markdown = require('markdown-it');
+const posthtml = require('posthtml');
+const posthtmlExpressions = require('posthtml-expressions');
+const posthtmInclude = require('posthtml-include');
+const posthtmlStyleExpansion = require('posthtml-style-expansion');
+const htmlMinifier = require('html-minifier');
 
 const environment = process.env.SITE_ENV || 'web';
 
-const mdExtLinks = md => md.replace(/<a(.+?)>/g, '<a$1 target="_blank" rel="noopener">');
-const environmentFilter = item => environment === 'web' ? item.web ?? true : environment === 'print' ? item.print ?? true : true;
+const getFilesInDir = dir => readdirSync(dir, { withFileTypes: true }).flatMap(file => file.isDirectory()
+    ? getFilesInDir(join(dir, file.name))
+    : join(dir, file.name));
 
-// Load in the main config yaml
-const config = parse(readFileSync(path.join(__dirname, 'data', 'config.yaml'), 'utf8'));
-config.meta.description = config.meta.description.replace(/\s*[\r\n]\s*/g, ' ');
-config.meta.keywords = config.meta.keywords.join(', ');
-config.footer.subtitle = config.footer.subtitle.replace(/{{YEAR}}/g, new Date().getFullYear());
-for (const link of config.contact.links) {
-    link.title = mdExtLinks(md.renderInline(link.title).replace(/<(\/?)em>/g, '<$1small>'));
-}
-for (const item of config.main) {
-    for (const item2 of item.items) {
-        item2.title = mdExtLinks(md.renderInline(item2.title));
+const buildScss = async () => {
+    // Get all the files in the directory recursively
+    const files = getFilesInDir(join(__dirname, 'scss'))
+        .filter(file => file.endsWith('.scss') && !file.split('/').slice(-1)[0].startsWith('_'));
+
+    // Compile all the files
+    const results = await Promise.all(files.map(file => sass.compileAsync(file, { sourceMap: true })
+        .then(res => ({ file, css: res.css, map: res.sourceMap }))));
+
+    // Export the results
+    for (const result of results) {
+        // Ensure the dir exists
+        const file = result.file.slice(join(__dirname, 'scss').length + 1);
+        const dir = dirname(join(__dirname, 'build', 'css', file));
+        if (!existsSync(dir)) mkdirSync(dir, { recursive: true });
+
+        // Write the CSS
+        writeFileSync(
+            join(__dirname, 'build', 'css', file.replace(/\.scss$/, '.css')),
+            `${result.css}\n/*# sourceMappingURL=${file.split('/').slice(-1)[0].replace(/\.scss$/, '.css.map')} */`,
+            { flag: 'w+' }
+        );
+
+        // Write the map
+        result.map.sources = result.map.sources.map(source => source.replace(/^file:\/\//, '').slice(join(__dirname, 'scss').length + 1));
+        writeFileSync(
+            join(__dirname, 'build', 'css', file.replace(/\.scss$/, '.css.map')),
+            JSON.stringify(result.map),
+            { flag: 'w+' }
+        );
     }
-}
-for (const item of config.rail) {
-    item.content = mdExtLinks(md.render(item.content));
-}
-config.main = config.main.filter(environmentFilter);
+};
 
-// Load in all the projects
-const projects = parse(readFileSync(path.join(__dirname, 'data', 'projects.yaml'), 'utf8')).filter(environmentFilter);
-for (const project of projects) {
-    project.slug = project.title.toLowerCase()
-        .replace(/^[a-z0-9-_]/g, '-')
-        .replace(/-{2,}/g, '-')
-        .replace(/(^-+|-+$)/g, '');
-    project.headline = mdExtLinks(md.renderInline(project.headline));
-    project.desc = mdExtLinks(md.render(project.desc));
-}
+const buildJs = async () => {
+    // Get all the files in the directory recursively
+    const files = getFilesInDir(join(__dirname, 'js'))
+        .filter(file => file.endsWith('.js') && !file.split('/').slice(-1)[0].startsWith('_'));
 
-// Load all the PostHTML requirements
-const posthtml = require('posthtml');
-const expressions = require('posthtml-expressions');
-const include = require('posthtml-include');
-const styleExpansion = require('posthtml-style-expansion');
+    // Bundle the files
+    await esbuild.build({
+        entryPoints: files,
+        bundle: true,
+        write: true,
+        minify: true,
+        sourcemap: true,
+        outdir: join(__dirname, 'build', 'js'),
+        outbase: join(__dirname, 'js'),
+        plugins: [ esbuildYaml.yamlPlugin() ]
+    });
+};
 
-// Get the source html
-const html = readFileSync(path.join(__dirname, 'templates', 'index.html'), 'utf8');
 
-// Render it
-posthtml([
-    include({ encoding: 'utf8' }),
-    expressions({ locals: { environment, config, projects } }),
-    environment === 'print' ? styleExpansion({ root: path.join(__dirname, 'build'), encoding: 'utf-8' }) : null
-].filter(x => !!x))
-    .process(html)
-    .then((result) => writeFileSync(
-        path.join(__dirname, 'build', `${environment === 'print' ? 'print' : 'index'}.html`),
-        result.html,
+const buildHtml = async () => {
+    const md = markdown();
+    const mdExtLinks = md => md.replace(/<a(.+?)>/g, '<a$1 target="_blank" rel="noopener">');
+    const environmentFilter = item => environment === 'web' ? item.web ?? true : environment === 'print' ? item.print ?? true : true;
+
+    // Load in the main config yaml
+    const config = yaml.parse(readFileSync(join(__dirname, 'data', 'config.yaml'), 'utf8'));
+    config.meta.description = config.meta.description.replace(/\s*[\r\n]\s*/g, ' ');
+    config.meta.keywords = config.meta.keywords.join(', ');
+    config.footer.subtitle = config.footer.subtitle.replace(/{{YEAR}}/g, new Date().getFullYear());
+    for (const link of config.contact.links) {
+        link.title = mdExtLinks(md.renderInline(link.title).replace(/<(\/?)em>/g, '<$1small>'));
+    }
+    for (const item of config.main) {
+        for (const item2 of item.items) {
+            item2.title = mdExtLinks(md.renderInline(item2.title));
+        }
+    }
+    for (const item of config.rail) {
+        item.content = mdExtLinks(md.render(item.content));
+    }
+    config.main = config.main.filter(environmentFilter);
+
+    // Load in all the projects
+    const projects = yaml.parse(readFileSync(join(__dirname, 'data', 'projects.yaml'), 'utf8')).filter(environmentFilter);
+    for (const project of projects) {
+        project.slug = project.title.toLowerCase()
+            .replace(/^[a-z0-9-_]/g, '-')
+            .replace(/-{2,}/g, '-')
+            .replace(/(^-+|-+$)/g, '');
+        project.headline = mdExtLinks(md.renderInline(project.headline));
+        project.desc = mdExtLinks(md.render(project.desc));
+    }
+
+    // Get the source html
+    const html = readFileSync(join(__dirname, 'templates', 'index.html'), 'utf8');
+
+    // Render it
+    const rendered = await posthtml([
+        posthtmInclude({ encoding: 'utf8' }),
+        posthtmlExpressions({ locals: { environment, config, projects } }),
+        environment === 'print' ? posthtmlStyleExpansion({ root: join(__dirname, 'build'), encoding: 'utf-8' }) : null
+    ].filter(x => !!x)).process(html).then(result => result.html);
+
+    // Minify it
+    const minified = htmlMinifier.minify(rendered, {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        continueOnParseError: true,
+        decodeEntities: true,
+        includeAutoGeneratedTags: false,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        sortAttributes: true,
+        sortClassName: true
+    });
+
+    // Write the file
+    writeFileSync(
+        join(__dirname, 'build', `${environment === 'print' ? 'print' : 'index'}.html`),
+        minified,
         { flag: 'w+' }
-    ));
+    );
+};
+
+buildScss()
+    .then(() => buildJs())
+    .then(() => buildHtml())
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
